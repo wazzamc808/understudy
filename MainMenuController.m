96dc2c7287e1ee68588a8efd8350653cfcfb593e
kirk kelsey <kirk.kelsey@0x4b.net>
2008-12-07 19:27:12 +0000

    Adding a copy of the GPL licenses and updating the file preambles to reflect the licensing.

--- MainMenuController.m
+++ MainMenuController.m
@@ -1,10 +1,20 @@
 //
-//  MainMenuController.m
-//  Understudy FR Appliance
+//  Copyright 2008 Kirk Kelsey.
 //
-//  Created by Kirk Kelsey.
-//  Copyright 2008. All rights reserved.
+//  This file is part of Understudy.
 //
+//  Understudy is free software: you can redistribute it and/or modify it under
+//  the terms of the GNU Lesser General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option)
+//  any later version.
+//
+//  Understudy is distributed in the hope that it will be useful, but WITHOUT 
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+//  for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with Understudy.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "FeedMenuController.h"
 #import "MainMenuController.h"
