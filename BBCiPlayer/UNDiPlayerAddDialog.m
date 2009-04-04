6eafc8d066c23fc4e2c55be64414b7f56794f50e
kirk kelsey <kirk.kelsey@0x4b.net>
2009-04-04 20:42:06 +0000

    creating a dialog to add iPlayer feeds

---
+++ BBCiPlayer/UNDiPlayerAddDialog.m
@@ -0,0 +1,92 @@
+//
+//  Copyright 2009 Kirk Kelsey.
+//
+//  This file is part of Understudy.
+//
+//  Understudy is free software: you can redistribute it and/or modify it under
+//  the terms of the GNU Lesser General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option)
+//  any later version.
+//
+//  Understudy is distributed in the hope that it will be useful, but WITHOUT 
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+//  for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with Understudy.  If not, see <http://www.gnu.org/licenses/>.
+
+#import "UNDiPlayerAddDialog.h"
+#import <MainMenuController.h>
+
+#import <BRControllerStack.h>
+#import <BRListControl.h>
+#import <BRTextMenuItemLayer.h>
+
+@implementation UNDiPlayerAddDialog
+
+/* feeds.bbc.co.uk/iplayer/categories/childrens/list
+Children's, Comedy, Drama, Entertainment, Factual, Films, Learning, Music
+News, Religion & Ethics, Sport, Sign Zone, Northern Ireland, Scotland, Wales */
+
+- (id)init
+{
+  [super init];
+  [self setTitle:@"BBC Feeds"];
+  feeds_ = [[NSMutableArray arrayWithObjects:@"http://feeds.bbc.co.uk/iplayer/bbc_one/list"
+             @"http://feeds.bbc.co.uk/iplayer/bbc_two/list",
+             @"http://feeds.bbc.co.uk/iplayer/bbc_three/list",
+             @"http://feeds.bbc.co.uk/iplayer/bbc_four/list",
+             @"http://feeds.bbc.co.uk/iplayer/cbbc/list",
+             @"http://feeds.bbc.co.uk/iplayer/cbeebies/list",
+             @"http://feeds.bbc.co.uk/iplayer/bbc_news24/list",
+             @"http://feeds.bbc.co.uk/iplayer/bbc_parliament/list",
+             @"http://feeds.bbc.co.uk/iplayer/bbc_alba/list",
+             nil] retain];
+  titles_ = [[NSMutableArray arrayWithObjects: @"BBC 1", 
+              @"BBC 2", 
+              @"BBC 3", 
+              @"BBC 4", 
+              @"CBBC", 
+              @"CBeebies", 
+              @"BBC News", 
+              @"Parliament",
+              @"BBC Alba", nil] retain];
+  [[self list] setDatasource:self];
+  return self;
+}
+
+- (void)dealloc
+{
+  [titles_ release];
+  [feeds_ release];
+  [super dealloc];
+}
+
+// call-back for an item having been selected
+- (void)itemSelected:(long)index
+{  
+  if( index < [feeds_ count] ){
+    UNDPreferenceManager* pref = [UNDPreferenceManager sharedInstance];
+    [pref addFeed:[feeds_ objectAtIndex:index] 
+        withTitle:[titles_ objectAtIndex:index]];
+  }
+  [[self stack] popController];
+}
+
+# pragma mark BRMenuListItemProvider
+
+- (long)itemCount{ return [feeds_ count]; }
+- (float)heightForRow:(long)row{ return 0; }
+- (BOOL)rowSelectable:(long)row{ return YES; }
+- (id)titleForRow:(long)row{ return [titles_ objectAtIndex:row]; }
+
+- (id)itemForRow:(long)row
+{
+  BRTextMenuItemLayer* item;
+  item = [BRTextMenuItemLayer menuItem];
+  [item setTitle:[self titleForRow:row]];
+  return item;
+}
+
+@end
