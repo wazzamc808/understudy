7856cbed65ea6fc55f0087713def7d4879731287
kirk kelsey <kirk.kelsey@0x4b.net>
2010-01-15 03:29:49 +0000

    use the separate netflix player to view video

--- Netflix/NetflixController.m
+++ Netflix/NetflixController.m
@@ -28,6 +28,7 @@
 #import <BRControllerStack.h>
 #import <BRDisplayManager.h>
 #import <BREvent.h>
+#import <BREventManager.h>
 #import <BRRenderScene.h>
 #import <BRSentinel.h>
 #import <BRSettingsFacade.h>
@@ -54,123 +55,32 @@
 {
   [pluginControl_ release];
   [asset_ release];
+  [player_ release];
   [super dealloc];
 }
 
 - (void)_loadVideo
 {
-  // invoking shared application ensures that windows can be ordered
-  [NSApplication sharedApplication];
-  NSRect rect = [[UNDPreferenceManager screen] frame];
-  mainView_ = [[[WebView alloc] initWithFrame:rect] retain];
-  [[[mainView_ mainFrame] frameView] setAllowsScrolling:NO];
-  [mainView_ setFrameLoadDelegate:self];
-  window_ = [[[NSWindow alloc] initWithContentRect:rect 
-                                         styleMask:0 
-                                           backing:NSBackingStoreBuffered 
-                                             defer:YES] retain];
-  [window_ setContentView:mainView_];
-  NSURLRequest* pageRequest = [NSURLRequest requestWithURL:[asset_ url]];
-  [mainView_ setCustomUserAgent:AGENTSTRING];
-  [[mainView_ mainFrame] loadRequest:pageRequest];
-  pluginControl_ = [[UNDPluginControl alloc] initWithView:mainView_];
+  // load the NetflixPlayer to actually play the video
+  NSString* path = @"/System/Library/CoreServices/Front Row.app/Contents/Plug"\
+  "Ins/frUnderstudy.frappliance/Contents/SharedSupport/NetflixPlayer.app/Conte"\
+  "nts/MacOS/NetflixPlayer";
+  NSString* url = [[asset_ url] absoluteString];
+  NSArray* args = [[NSArray alloc] initWithObjects:@"--FR", url, nil];
+  if( [[NSFileManager defaultManager] fileExistsAtPath:path] )
+    player_ = [[NSTask launchedTaskWithLaunchPath:path arguments:args] retain];
+  else
+    NSLog (@"NetflixPlayer not available");
+  // TODO: need to provide a meaningful error dialog here
+  
+  // create an event to mimic a spurious key press, causing Front Row to drop
+  // out to the loaded application. this is a reasonable compromize between
+  // ordering out or destroying the scene (which doesn't really give up user
+  // input to the new app) and terminating FR completely.
+  BREvent* ev = [[BREvent alloc] initWithPage:1 usage:136 value:1];
+  [[BREventManager sharedManager] postEvent:ev];
 }
 
-- (BOOL)signIn
-{
-  static int tried = false;
-  if( tried ) return NO;
-  tried = true;
-  
-  WebScriptObject* script = [mainView_ windowScriptObject];
-  NSString *setUser, *setPass, *submit;
-  NSString* user = [UNDPreferenceManager accountForService:@"www.netflix.com"];
-  [[user retain] autorelease];
-  if( !user ) return NO;
-  
-  NSString* pass = [UNDPasswordProvider passwordForService:@"www.netflix.com"
-                                                   account:user];
-  [[pass retain] autorelease];
-
-  if( !user ) return NO;
-  
-  setUser = @"document.getElementsByName('email').item(0).value = '%@'";
-  setPass = @"document.getElementsByName('password1').item(0).value = '%@'";
-  submit = @"document.getElementsByName('login_form').item(0).submit()";
-  setUser = [NSString stringWithFormat:setUser,user];
-  setPass = [NSString stringWithFormat:setPass,pass];
-  [script evaluateWebScript:setUser];
-  [script evaluateWebScript:setPass]; 
-  [script evaluateWebScript:submit];
-  return YES;
-}
-
-// <WebFrameLoadDelegate> callback once the video loads
-- (void)webView:(WebView*)view didFinishLoadForFrame:(WebFrame*)frame
-{
-  if( frame != [view mainFrame] ) return;
-  
-  [window_ display];
-  
-  // if there isn't a plugin, perhaps we need to signin
-  if( ![pluginControl_ plugin] )
-  {
-    if( [self signIn] ) return;
-    if( ![UNDPreferenceManager alertsAreDisabled] )
-    {
-      NSString* title = @"Error";
-      NSString* primary = @"Video Could Not Be Loaded";
-      NSString* secondary = @"Please ensure that you are logged into your Netfli"\
-      "x account in Safari, and that you have not reached your viewing limit.";
-      BRAlertController* alert = [BRAlertController alertOfType:3
-                                                         titled:title
-                                                    primaryText:primary
-                                                  secondaryText:secondary];
-      [[self stack] swapController:alert];      
-      return;
-    }
-  }
-
-  [window_ display];
-  [window_ orderFrontRegardless];
-  [window_ setLevel:NSScreenSaverWindowLevel];
-  [self reveal];      
-}
-
-- (void)fullscreen
-{
-  if( ![pluginControl_ plugin] ){
-    NSLog(@"cannot fullscreen netflix player (no plugin available)");
-    return;
-  }
-  
-  NSSize size = [pluginView_ frame].size;
-  
-  NSPoint fsPoint;
-  // the fullscreen button is 30px high, and 30px up from the bottom edge
-  fsPoint.y = size.height -  (30 + 30/2);
-  // the fullscreen button is 15px in from the right edge (and 70px wide)
-  fsPoint.x = size.width - (15 + 75/2);
-  // if the player is more than 1000px wide, padding is added
-  if( size.width > 1000 ) fsPoint.x -= (size.width-1000)/2;
-
-  
-  [pluginControl_ sendPluginMouseClickAtPoint:fsPoint];
-  [pluginControl_ sendPluginMouseClickAtPoint:fsPoint];
-  
-}
-
-# pragma mark Player Controls
-
-- (void)playPause
-{
-  [pluginControl_ sendPluginKeyCode:49 withCharCode:0]; // space-bar
-}
-
-- (void)fastForward
-{
-  [self fullscreen];
-}
 
 #pragma mark BR Control
 
