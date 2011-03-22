1c886c45bed159efd7aa3458f8524b50cd0f5a30
Kirk Kelsey <kirk.kelsey@0x4b.net>
2011-03-21 21:38:15 -0400

    add a utility function to provide the appliance icon
    * Utilities/UNDIconProvider.h: New file.
    
    * Utilities/UNDIconProvider.m: New file.
    
    * Menuing/UNDManageDialog.m: Set the dialog's icon.
    
    * understudy.xcodeproj/project.pbxproj: Add new files.

---
+++ Utilities/UNDIconProvider.h
@@ -0,0 +1,24 @@
+//
+//  Copyright 2011 Kirk Kelsey.
+//
+//  This file is part of Understudy.
+//
+//  Understudy is free software: you can redistribute it and/or modify it under
+//  the terms of the GNU Lesser General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option)
+//  any later version.
+//
+//  Understudy is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+//  for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with Understudy. If not, see <http://www.gnu.org/licenses/>.
+
+#import <Foundation/Foundation.h>
+
+#import "BRImage.h"
+
+BRImage *UNDIcon(void);
+NSString *UNDIconID(void);
